8ea5183b758f32eb16847d00d521be32ebfe223e
reza <reza16.sugiarto@gmail.com>
2024-11-19 13:03:25 +0700

    Update dockerfile

--- ollama/run_ollama.sh
+++ ollama/run_ollama.sh
@@ -1,14 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# echo "Starting Ollama server..."
-# ollama serve &
-# ollama run llama3.2:1b
-
-
-# echo "Waiting for Ollama server to be active..."
-# while [ "$(ollama list | grep 'NAME')" == "" ]; do
-    # sleep 1
-# done
-
-ollama create model -f ./Modelfile &
-ollama run model
+ollama serve &
+ollama list
+ollama pull llama3.2:1b
